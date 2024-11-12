3ad1ab13caaa41ac8949e72eab9852cc8d1eea3e
HuHuKey <1115152676@qq.com>
2024-11-12 10:36:43 +0800

    实现了从DataFrame或者Dict[str,list[str]]的格式去重地存入MongoDB的方法。

--- 数据采集与数据存储/爬虫程序/datasaver/MongoSaver.py
+++ 数据采集与数据存储/爬虫程序/datasaver/MongoSaver.py
@@ -2,10 +2,25 @@ from pymongo import MongoClient
 import pandas
 
 
+def transToListOfLists(d: dict[str, list[str]]):
+    return [{k: v for k, v in zip(d.keys(), values)} for values in zip(*d.values())]
+
+
 def saveDataFrameToMongoDB(df: pandas.DataFrame, db_name: str, collection: str,
                            host: str = "mongodb://localhost:27017/"):
     client = MongoClient(host)
     db = client[db_name]
     collection = db[collection]
     data_dict = df.to_dict("records")
-    collection.insert_many(data_dict)
+    for upsert in data_dict:
+        collection.update_one(upsert, {"$set": upsert}, upsert=True)
+
+
+def saveDictToMongoDB(d: dict[str, list[str]], db_name: str, collection: str,
+                      host: str = "mongodb://localhost:27017/"):
+    client = MongoClient(host)
+    db = client[db_name]
+    collection = db[collection]
+    tuples = [{k: v for k, v in zip(d.keys(), values)} for values in zip(*d.values())]
+    for t in tuples:
+        collection.update_one(t, {"$set": t}, upsert=True)
