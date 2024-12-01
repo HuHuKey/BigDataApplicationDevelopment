072f52a6796284c5a791c0c8165615385b52d0b3
HuHuKey <1115152676@qq.com>
2024-12-01 23:18:10 +0800

    更新了一下爬取样例

--- 数据采集与数据存储/爬虫程序/example.py
+++ 数据采集与数据存储/爬虫程序/example.py
@@ -1,25 +1,20 @@
-import pandas as pd
-import json
-from crawler.Crawler import JDCrawler,TBCrawler
-from datasaver.MongoSaver import saveDataFrameToMongoDB,saveDictToMongoDB
+from crawler.Crawler import JDCrawler, TBCrawler
+from datasaver.MongoSaver import saveDataFrameToMongoDB, saveDictToMongoDB
 
 if __name__ == "__main__":
     url1 = "http://www.jd.com"
     url2 = "http://www.taobao.com"
-    keywords = input("请输入搜索关键词：")
-    page = int(input("请输入爬取页数："))
-    # crawler1= JDCrawler(name="JD", url=url1, cookie_filename='../conf/USER_COOKIES.txt')
-    # crawler1.searchKeyWords(keywords=keywords)
-    # dt1 = crawler1.getDictOfGoods(page)
-    # df1 = pd.DataFrame(dt1)
-    # df1.to_csv('./data/jd/' + keywords[:3] + '.csv', encoding='utf-8')
-    # saveDictToMongoDB(dt1, 'jd', keywords[::])
-    # print("京东：", df1)
+    keywords = ["西瓜"]
+    page = 10
+    crawler1 = JDCrawler(name="JD", cookie_filename='../conf/USER_COOKIES.txt')
+    crawler1.searchKeyWords(keywords=keywords)
+    dt1 = crawler1.getDataFrameOfGoods(page)
+    # crawler2 = TBCrawler(name="TB", cookie_filename='../conf/USER_COOKIES.txt')
+    # crawler2.searchKeyWords(keywords=keywords)
+    # dt2 = crawler2.getDictOfGoods(page)
 
-    crawler2 = TBCrawler(name="TB", url=url2, cookie_filename='../conf/USER_COOKIES.txt')
-    crawler2.searchKeyWords(keywords=keywords)
-    dt2 = crawler2.getDictOfGoods(page)
-    df2 = pd.DataFrame(dt2)
-    df2.to_csv('./data/tb/'+keywords[:3]+'.csv',encoding='utf-8')
-    saveDictToMongoDB(dt2, 'tb', keywords[::])
-    print("淘宝：", df2)
+    # print(dt1)
+    # print(dt2)
+    saveDataFrameToMongoDB(dt1, 'jd', 'goods')
+    # saveDictToMongoDB(dt2, 'tb', keywords[:3])
+    # save_to_csv(dt2,keywords[:3])
