a07a3a81a2294680e2c193775c942c7f2e2a45d9
HuHuKey <1115152676@qq.com>
2024-12-07 09:59:10 +0800

    优化JDCrawler构造，移除cookie文件参数

--- 数据采集与数据存储/爬虫程序/example.py
+++ 数据采集与数据存储/爬虫程序/example.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     url2 = "http://www.taobao.com"
     keywords = ["西瓜"]
     page = 10
-    crawler1 = JDCrawler(name="JD", cookie_filename='../conf/USER_COOKIES.txt')
+    crawler1 = JDCrawler(name="JD")
     crawler1.searchKeyWords(keywords=keywords)
     dt1 = crawler1.getDataFrameOfGoods(page)
     # crawler2 = TBCrawler(name="TB", cookie_filename='../conf/USER_COOKIES.txt')
