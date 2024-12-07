d6378811a0997b533c2eea1012caebeab67a524e
HuHuKey <1115152676@qq.com>
2024-12-07 09:57:44 +0800

    优化Cookie管理，支持MongoDB持久化

--- 数据采集与数据存储/爬虫程序/crawler/cookies/CookieSaver.py
+++ 数据采集与数据存储/爬虫程序/crawler/cookies/CookieSaver.py
@@ -4,16 +4,22 @@ from selenium.webdriver.chrome import webdriver
 import json
 import time
 import pymongo
-from selenium.webdriver.chrome.options import Options
 
 
 class Cookie:
-    content = None
     domain = None
+    content = None
 
-    def __init__(self, content: dict, domain: str):
-        self.content = content
+    def __init__(self, domain: str, content: dict = None, method="mongodb"):
         self.domain = domain
+        if self.content is None:
+            switch = {
+                "mongodb": self.read_mongoDB,
+                "json": self.read_json,
+            }
+            switch.get(method)()
+        else:
+            self.content = content
 
     def to_dict(self):
         return {self.domain: self.content}
@@ -47,59 +53,77 @@ class Cookie:
         """
         return not (self.is_cookie_empty() or self.is_cookie_expired())
 
-    def to_mongo(self, host="mongodb://localhost:27017/"):
+    def to_mongoDB(self, host="mongodb://localhost:27017/", database="cookie", collection="cookies"):
         with pymongo.MongoClient(host) as client:
-            db = client['cookie']
-            collection = db[self.domain]
-            collection.update_one({'domain': self.domain}, {'$set': self.content}, upsert=True)
+            db = client[database]
+            collection = db[collection]
+            collection.update_one({'domain': self.domain}, {'$set': {'content': self.content}}, upsert=True)
+
+    def read_mongoDB(self, host="mongodb://localhost:27017/", database="cookie", collection="cookies"):
+        with pymongo.MongoClient(host) as client:
+            db = client[database]
+            collection = db[collection]
+            result = collection.find_one({'domain': self.domain})
+            if result is not None:
+                self.content = result['content']
+
+    def read_json(self, filepath):
+        with open(filepath, "r") as fp:
+            self.content = json.load(fp)
 
 
 class CookieSaver:
     __driver: webdriver = None
-    __cookies = None
+    __cookies: Cookie = None
     filename = None
 
-    def __init__(self, driver: webdriver, filename):
+    def __init__(self, driver: webdriver):
         self.__driver = driver
-        self.filename = filename
+        self.__cookies = Cookie(self.get_current_domain())
 
-    def get_cookies(self):
+    def refresh_cookies(self):
         """
         获取当前网页的cookie
         :return:
         """
-        self.__cookies = self.__driver.get_cookies()
-        # print(self.__cookies)
+        self.refresh_domain()
+        self.__cookies.content = self.__driver.get_cookies()
+
+    def get_current_domain(self):
+        domain = self.__driver.execute_script("return window.location.hostname;")
+        domain = domain.replace("www.", "")
+        return domain
+
+    def refresh_domain(self):
+        self.__cookies.domain = self.get_current_domain()
 
     # TODO: 实现cookie的mongoDB存取
     # TODO: 以{'url':'cookie'}的形式存储cookie,以{'url':'cookie'}的形式加载cookie
-    def save_cookies(self):
+    def save_cookies(self, method="mongodb"):
         """
-        持久化当前网页cookie
+        持久化当前网页cookie,用mongoDB存储，文件存储不安全，已弃用
         :return:
         """
-        domain = self.__driver.execute_script("return window.location.hostname;")
-        domain = domain.replace("www.", "")
-        temp = {domain: self.__cookies}
-        with open(self.filename, 'w') as f:
-            json.dump(self.__cookies, fp=f, indent=4)
+        self.refresh_cookies()
+        self.__cookies.to_mongoDB()
 
     def load_cookies(self):
         """
         加载持久化cookie
         :return: 是否成功加载cookie
         """
-        try:
-            with open(self.filename, "r") as fp:
-                self.__cookies = json.load(fp)
-        except Exception as e:
-            warnings.warn(f"加载cookie失败: {e}")
-            return False
-        if self.is_cookie_empty():
+        self.__cookies.read_mongoDB()
+        # try:
+        #     with open(self.filename, "r") as fp:
+        #         self.__cookies = json.load(fp)
+        # except Exception as e:
+        #     warnings.warn(f"加载cookie失败: {e}")
+        #     return False
+        if self.__cookies.is_cookie_empty():
             return False
         try:
             self.__driver.delete_all_cookies()
-            for cookie in self.__cookies:
+            for cookie in self.__cookies.content:
                 self.__driver.add_cookie(cookie)
             self.__driver.refresh()
         except Exception as e:
@@ -112,23 +136,22 @@ class CookieSaver:
         判断cookie是否过期
         :return: cookie是否过期
         """
-
-        current_time = int(time.time())
-        for cookie in self.__cookies:
-            if cookie.get('expire', current_time + 1) < current_time:
-                return True
-        return False
+        return self.__cookies.is_cookie_expired()
 
     def is_cookie_empty(self):
         """
         判断cookie是否为空
         :return: cookie是否为空
         """
-        return self.__cookies is None or len(self.__cookies) == 0 or self.__cookies == []
+        return self.__cookies.is_cookie_empty()
 
     def is_cookie_valid(self):
         """
         判断cookie是否有效
         :return: cookie是否有效
         """
-        return not (self.is_cookie_empty() or self.is_cookie_expired())
+        return self.__cookies.is_cookie_valid()
+
+    @property
+    def cookies(self):
+        return self.__cookies
