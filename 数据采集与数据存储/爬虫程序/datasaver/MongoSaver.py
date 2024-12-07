2122a87de538e890e5b15f15d436c642db36763a
HuHuKey <1115152676@qq.com>
2024-12-07 09:58:46 +0800

    优化MongoSaver连接管理，使用with语句

--- 数据采集与数据存储/爬虫程序/datasaver/MongoSaver.py
+++ 数据采集与数据存储/爬虫程序/datasaver/MongoSaver.py
@@ -8,19 +8,19 @@ def transToListOfLists(d: dict[str, list[str]]):
 
 def saveDataFrameToMongoDB(df: pandas.DataFrame, db_name: str, collection: str,
                            host: str = "mongodb://localhost:27017/"):
-    client = MongoClient(host)
-    db = client[db_name]
-    collection = db[collection]
-    data_dict = df.to_dict("records")
-    for upsert in data_dict:
-        collection.update_one(upsert, {"$set": upsert}, upsert=True)
+    with MongoClient(host) as client:
+        db = client[db_name]
+        collection = db[collection]
+        data_dict = df.to_dict("records")
+        for upsert in data_dict:
+            collection.update_one(upsert, {"$set": upsert}, upsert=True)
 
 
 def saveDictToMongoDB(d: dict[str, list[str]], db_name: str, collection: str,
                       host: str = "mongodb://localhost:27017/"):
-    client = MongoClient(host)
-    db = client[db_name]
-    collection = db[collection]
-    tuples = [{k: v for k, v in zip(d.keys(), values)} for values in zip(*d.values())]
-    for t in tuples:
-        collection.update_one(t, {"$set": t}, upsert=True)
+    with MongoClient(host) as client:
+        db = client[db_name]
+        collection = db[collection]
+        tuples = [{k: v for k, v in zip(d.keys(), values)} for values in zip(*d.values())]
+        for t in tuples:
+            collection.update_one(t, {"$set": t}, upsert=True)
