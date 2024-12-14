9fee074fe4fcad9a3e26a5bcd36427aedfb74838
white <1240896698@qq.com>
2024-12-14 16:49:22 +0800

    添加了前端应用

--- 数据可视化/BigDataApplication/EBDashBoard/views.py
+++ 数据可视化/BigDataApplication/EBDashBoard/views.py
@@ -15,7 +15,7 @@ from .models import Jdnew
 @login_required
 def dashboard_view(request):
     try:
-        sales_data = Jdnew.objects().all()
+        sales_data = Jdnew.objects().all()[:30]
         context = {'sales_data': sales_data}
         # all_sales_data = SalesData.objects()
         # print("查询到的数据数量:", len(all_sales_data))  # 添加调试输出
